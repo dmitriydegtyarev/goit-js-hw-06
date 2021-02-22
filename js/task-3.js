481bc451fbbeb4f4058a22b62f3d50d211c1eedc
Dmitriy Dihtiarov <dihtiarovd@gmail.com>
2021-02-22 21:32:52 +0200

    finish task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,35 +1,14 @@
-class Storage{
+import users from './users.js';
 
-  constructor(items) {
-    this.items = items;
-  }
+const getUsersWithGender = (users, gender) => {
+  const userNameByGender = [];
+  users.forEach(user => {
+    if (user.gender === gender) {
+      userNameByGender.push(user.name);
+    }  
+  });
 
-  getItems() {
-    return this.items;
-  }
+  return userNameByGender;
+};
 
-  addItem(item) {
-    this.items.push(item);
-  }
-
-  removeItem(item) {
-    const itemIndex = this.items.indexOf(item);
-    this.items.splice(itemIndex, 1);
-  }
-}
-
-const storage = new Storage([
-  'Нанитоиды',
-  'Пролонгер',
-  'Железные жупи',
-  'Антигравитатор',
-]);
-
-const items = storage.getItems();
-console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
-
-storage.addItem('Дроид');
-console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
-
-storage.removeItem('Пролонгер');
-console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
