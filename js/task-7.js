e6608ace659ce2c0ae6184a79bf2ff7423473c2b
Dmitriy Dihtiarov <dihtiarovd@gmail.com>
2021-02-22 22:42:23 +0200

    finish task 7

--- js/task-7.js
+++ js/task-7.js
@@ -1,7 +1,9 @@
 import users from './users.js';
 
 const calculateTotalBalance = users => {
-  // твой код
+
+  const totalBalance = users.reduce((totalBalance, user) => totalBalance + user.balance, 0);
+  return totalBalance;
 };
 
 console.log(calculateTotalBalance(users)); // 20916
