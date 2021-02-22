568c9f171cc1d93344b84395862e4405a4721a0a
Dmitriy Dihtiarov <dihtiarovd@gmail.com>
2021-02-23 01:15:36 +0200

    finish task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,9 +1,16 @@
 import users from './users.js';
 
 const getSortedUniqueSkills = users =>
-  users
-    .reduse()
-  // .sort((a, b) => a.skills.length - b.skills.length)
+  users.reduce((allSkills, user) => {
+    allSkills.push(...user.skills);
+
+    return allSkills;
+  }, [])
+    
+    .filter((skill, index, allSkills) => allSkills.indexOf(skill) === index)
+
+    .sort();
+    
 
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
