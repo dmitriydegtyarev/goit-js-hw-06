fe45510cf019f7e77d6803ee4f8abc75a9d82fb0
Dmitriy Dihtiarov <dihtiarovd@gmail.com>
2021-03-09 15:30:53 +0200

    fix task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,7 @@
 import users from './users.js';
 
 const getUsersWithEyeColor = (users, color) => {
-  const colorEyes = [];
-  users.forEach(user => {
-    if (user.eyeColor === color) {
-      colorEyes.push(user);
-    }
-  });
-  return colorEyes; 
+  console.log(users.filter(user => user.eyeColor === color));
 };
 
-console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+getUsersWithEyeColor(users, 'blue'); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
